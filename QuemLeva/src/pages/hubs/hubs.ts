152b2c53312ff6587380473ad02cf3cc39a4e725
Lucas Azevedo <lucasazevedo9@gmail.com>
2018-04-08 04:41:51 -0300

    Dados mockados

--- QuemLeva/src/pages/hubs/hubs.ts
+++ QuemLeva/src/pages/hubs/hubs.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetalhesHubPage } from '../detalhes-hub/detalhes-hub';
 
 @Component({
   selector: 'page-hubs',
@@ -7,10 +8,66 @@ import { NavController } from 'ionic-angular';
 })
 export class HubsPage {
 
-  private itens: any = [
-    { nome: 'UFC', icon: 'school' },
-    { nome: 'iFactory', icon: 'briefcase' },
-    { nome: 'Iguatemy', icon: 'pricetag' }
+  private hubs: any = [
+    {
+      nome: 'UFC',
+      icon: 'school',
+      grupos: [
+        {
+          nome: "Carro do Neto",
+          membros: [
+            "Neto",
+            "Lucas",
+            "Lana",
+          ],
+          agenda: {
+            idas: [
+              { horario: '07:00', dias: ['seg', 'ter', 'qua', 'qui', 'sex'] },
+              { horario: '18:00', dias: ['seg', 'qua', 'sex'] },
+            ],
+            voltas: [
+              { horario: '12:00', dias: ['seg', 'ter', 'qua', 'qui', 'sex'] },
+              { horario: '22:00', dias: ['seg', 'qua', 'sex'] },
+            ]
+          }
+        },
+        {
+          nome: "Carro do Mackeenzy",
+          membros:[
+            "Edson"
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'iFactory',
+      icon: 'briefcase',
+      grupos: [
+        {
+          nome: "Carro do Mikael",
+          membros: [
+            "Mikael",
+            "Lana",
+            "Neto",
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Iguatemi',
+      icon: 'pricetag',
+      grupos: [
+        {
+          nome: "Carro do Neto",
+          membros: [
+            "Lana",
+            "Lucas",
+            "Neto",
+            "Mikael",
+          ]
+        }
+      ]
+    }
   ];
 
   constructor(public navCtrl: NavController) {
@@ -25,7 +82,11 @@ export class HubsPage {
     
   }
 
-  itemSelected(item) {
-    console.log(item);
+  onSubmit(event) {
+    
+  }
+
+  itemSelected(hub) {
+    this.navCtrl.push(DetalhesHubPage, {hub: hub});
   }
 }
