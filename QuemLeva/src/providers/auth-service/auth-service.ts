3730abe02fa1191b53c315d1197b946f5c6dadfb
Mikael Lemos <mikaellemos@alu.uc.br>
2018-04-09 22:32:08 -0300

    facebook

--- QuemLeva/src/providers/auth-service/auth-service.ts
+++ QuemLeva/src/providers/auth-service/auth-service.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'ionic-angular';
 import { User } from './../../models/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -7,6 +8,7 @@ import * as firebase from 'firebase';
 import { BaseServiceProvider } from '../base-service/base-service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook';
 
 /*
   Generated class for the AuthServiceProvider provider.
@@ -20,15 +22,39 @@ export class AuthServiceProvider extends BaseServiceProvider {
   constructor(
     public af: AngularFireAuth,
     public db: AngularFireDatabase,
-    public http: HttpClient) {
+    public http: HttpClient,
+    private facebook: Facebook,
+    private platform: Platform,
+) {
       super()
       console.log('Hello AuthServiceProvider Provider');
   }
 
-  loginFacebook(): Promise<boolean> {
+  loginFacebook() {
     var provider = new firebase.auth.FacebookAuthProvider();
-    return new Promise((resolve, reject) => {
+
+    if (this.platform.is('cordova')) {
+      this.facebook.login(['email', 'public_profile'])
+      .then(res => {
+      const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+      firebase.auth().signInWithCredential(facebookCredential);
+      const userObservable = this.db.object(`/users/${this.af.auth.currentUser.uid}`);
+        userObservable.first().subscribe((user: User) => {
+          if(user.hasOwnProperty('$value')){
+            userObservable.update({
+              name: this.af.auth.currentUser.displayName, 
+              email: this.af.auth.currentUser.email, 
+              facebookToken: res.authResponse.accessToken, 
+              uidFacebook: res.authResponse.userID,
+              photo: `https://graph.facebook.com/${res.authResponse.userID}/picture?type=large&access_token=${res.authResponse.accessToken}`}).then(() => {
+              }).catch(this.handlePromiseError); 
+          }
+        })
+      })
+    }
+    else {
       firebase.auth().signInWithPopup(provider).then((result) => {
+        console.log(result);
         const userObservable = this.db.object(`/users/${result.user.uid}`);
         userObservable.first().subscribe((user: User) => {
           if(user.hasOwnProperty('$value')){
@@ -38,12 +64,11 @@ export class AuthServiceProvider extends BaseServiceProvider {
               facebookToken: result.credential.accessToken, 
               uidFacebook: result.additionalUserInfo.profile.id,
               photo: `https://graph.facebook.com/${result.additionalUserInfo.profile.id}/picture?type=large&access_token=${result.credential.accessToken}`}).then(() => {
-                resolve(true);
               }).catch(this.handlePromiseError); 
           }
         })
       }).catch(this.handlePromiseError);
-    })
+    }
   }
 
   createAuthUser(user: {email : string, password : string}) : firebase.Promise<AngularFireAuth>{
